da303f523a6070246b54f20e9e5e655d552d48fc
muahmad-hub <hi.ahmad.pro@gmail.com>
2025-08-15 11:15:34 +0300

    Remove previous app folder

--- alumni_website/alumni/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Users)
-admin.site.register(Profile)
-admin.site.register(Mentor)
-admin.site.register(Skills)
-admin.site.register(Languages)
-admin.site.register(MentorMatch)
