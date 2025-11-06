e32281cc2b7c973cca29fbf02d93d91687475b86
muahmad-hub <hi.ahmad.pro@gmail.com>
2025-11-06 18:44:30 +0300

    Add Database ping url

--- alumni_website/core/urls.py
+++ alumni_website/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("health_check/", views.health_check, name="health_check"),
+    path("db_health_check/", views.db_health_check, name="db_health_check")
 ]
--- alumni_website/core/views.py
+++ alumni_website/core/views.py
@@ -4,7 +4,7 @@ from profiles.models import Profile
 from django.contrib import messages
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.db import connection
 
 # View to handle homepage view
 # Alerts users if important information in profile hasn't been added
@@ -29,3 +29,12 @@ def home(request):
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
+
+def db_health_check(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1;")
+        return JsonResponse({"db": "ok"})
+    except Exception as e:
+        return JsonResponse({"db": "error", "detail": str(e)}, status=500)
+
